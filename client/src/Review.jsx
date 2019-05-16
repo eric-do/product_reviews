c905c8050f39f7212cc8dd32fb82025df5e71538
ericdo <ericdo.617@gmail.com>
2019-05-16 07:41:14 -0700

    Add continuous integration and further component styling

--- client/src/Review.jsx
+++ client/src/Review.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 const Review = (props) => (
   <div className="review-box">
@@ -19,12 +20,20 @@ const Review = (props) => (
     <div className="right-col">
     <div className="vote-header tooltip">
       <div className="thumb">
+        {/* { props.review.recommended ? <img src={require("../images/thumbs-up.png")} /> : <img src={require("../images/thumbs-down.png")} /> } */}
       </div>
       <div className="title">
-      { props.review.recommended ? `Recommended` : `Not recommended` }
+        { props.review.recommended ? 'Recommended' : 'Not recommended'}
       </div>
       <div className="hours">
+        { `${props.review.hours_played} hrs on record` }
       </div>
+    </div>
+    <div className="posted-date">
+        { `Posted ${ moment(props.review.review_date).format("MMM Do")}`}
+      </div>
+    <div className="content">
+      { `${(props.review.content)}`}
     </div>
     </div>
   </div>
