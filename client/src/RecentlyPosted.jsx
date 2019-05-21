07bb1a2c90bfe5a7c6202cc42f746c574c9f09a7
ericdo <ericdo.617@gmail.com>
2019-05-21 13:36:25 -0700

    Turned RecentlyPosted component into a stateless component

--- client/src/RecentlyPosted.jsx
+++ client/src/RecentlyPosted.jsx
@@ -1,50 +1,16 @@
 import React from 'react';
-import moment from 'moment';
 import styled from 'styled-components';
-import $ from 'jquery';
 import MiniReview from './MiniReview.jsx';
 
-class RecentlyPosted extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      reviews: [],
-      date: props.date || null
-    };
-  }
+const RecentlyPosted = ({reviews}) => ((
+  <ReviewsWrapper> 
+    <Title>RECENTLY POSTED</Title>
+    {
+      reviews.map(review => <MiniReview key={review.post_id} review={review}/>)
+    }
+  </ReviewsWrapper>
+));
 
-  componentDidMount() {
-    //this.getRecent();
-  }
-
-  getRecent() {
-    $.ajax({
-      url: 'http://localhost:3005/recent',
-      data: {
-        date: this.state.date, 
-        where: this.state.filters
-      },
-      method: 'GET',
-      success: (result) => {
-        let reviews = result.rows;
-        this.setState({ reviews });
-      },
-      // eslint-disable-next-line quotes
-      error: () => console.error(`Couldn't get reviews`)
-    });
-  }
-
-  render() {
-    return (
-      <ReviewsWrapper> 
-        <Title>RECENTLY POSTED</Title>
-        {
-          this.props.reviews.map(review => <MiniReview key={review.post_id} review={review}/>)
-        }
-      </ReviewsWrapper>
-    );
-  }
-}
 
 const Title = styled.div`
   padding-top: 20px;
